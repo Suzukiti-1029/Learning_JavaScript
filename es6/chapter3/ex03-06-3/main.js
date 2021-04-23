2a14b39a7f6c18c8fa36aa12c9a971e456388627
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-23 11:11:42 +0900

    数値

---
+++ es6/chapter3/ex03-06-3/main.js
@@ -0,0 +1,16 @@
+const small = Number.EPSILON;
+console.log(small);
+const bigInt = Number.MAX_SAFE_INTEGER;
+console.log(bigInt);
+const max = Number.MAX_VALUE;
+console.log(max);
+const minInt = Number.MIN_SAFE_INTEGER;
+console.log(minInt);
+const min = Number.MIN_VALUE;
+console.log(min);
+const nInf = Number.NEGATIVE_INFINITY;
+console.log(nInf);
+const nan = Number.NaN
+console.log(nan);
+const inf = Number.POSITIVE_INFINITY;
+console.log(inf);
