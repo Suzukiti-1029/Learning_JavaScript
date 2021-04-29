b50b669b05af3ddd7a72f293fb4cfffb7879ade4
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-29 21:55:03 +0900

    JavaScriptの制御フロー文/if..else文のつなぎ方

---
+++ es6/chapter4/ex04-02-1/main.js
@@ -0,0 +1,17 @@
+if(new Date().getDay() === 3) {
+  totalBet = 1;
+} else if (funds === 7) {
+  totalBet = funds;
+} else {
+  console.log("縁起担ぎはなし。");
+}
+
+if(new Date().getDay() === 3) {
+  totalBet = 1;
+} else {
+  if (funds === 7) {
+    totalBet = funds;
+  } else {
+    console.log("縁起担ぎはなし。");
+  }
+}
