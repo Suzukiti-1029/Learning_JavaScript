02478b35429e6442904fe0af062342c59c978486
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-05-11 16:29:57 +0900

    typeof演算子

---
+++ es6/chapter5/ex05-11-1/main.js
@@ -0,0 +1,12 @@
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof true);
+console.log(typeof 1);
+console.log(typeof "");
+console.log(typeof Symbol());
+console.log(typeof function(){});
+
+console.log("----");
+let a = (typeof undefined);
+console.log(typeof a);
