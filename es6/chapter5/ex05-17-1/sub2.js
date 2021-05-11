33ac481a82c7cdd4bc5735d18ea10c44b511f5f1
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-05-11 17:27:13 +0900

    if文の短絡論理ORの変換

---
+++ es6/chapter5/ex05-17-1/sub2.js
@@ -0,0 +1,13 @@
+let options = true;
+if (!options) options = {};
+console.log(options);
+
+options = options || {};
+console.log(options);
+
+options = false;
+if (!options) options = {};
+console.log(options);
+
+options = options || {};
+console.log(options);
