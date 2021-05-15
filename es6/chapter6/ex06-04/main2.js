ee141957874c1a0078434561a1fa59398b16db95
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-05-15 16:34:40 +0900

    メソッドの省略記法

---
+++ es6/chapter6/ex06-04/main2.js
@@ -0,0 +1,12 @@
+const o = {
+  name: 'Wallance',
+  bark() { return 'Woof!'; },
+};
+const o2 = {
+  名前: 'ポチ',
+  吠える() { return 'ウーッ、ワン！'; },
+};
+console.log(o);
+console.log(o2);
+console.log(o.bark());
+console.log(o2.吠える());
