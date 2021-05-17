55f6c5068397c6d626f98e472831306c5b38b1ea
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-05-17 10:27:05 +0900

    アロー関数

---
+++ es6/chapter6/ex06-07/sub1.js
@@ -0,0 +1,8 @@
+const f1 = function() { return "hello!"; }
+const f1 = () => "hello!";
+
+const f2 = function(name) { return `Hello,${name}!`; }
+const f2 = name => `Hello, ${name}!`;
+
+const f3 = function(a, b) { return a + b }
+const f3 = (a, b) => a + b;
