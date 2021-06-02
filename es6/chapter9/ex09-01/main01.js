777386f604162692e4eb611b21a465804b44ffcb
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-02 10:28:01 +0900

    for...in

---
+++ es6/chapter9/ex09-01/main01.js
@@ -0,0 +1,17 @@
+const SYM = Symbol();
+const o = { a: 1, b: 2, c: 3, [SYM]: 4 };
+for (const prop in o) {
+  if (!o.hasOwnProperty(prop)) continue;
+  console.log(`${prop}: ${o[prop]}`);
+}
+
+const o2 = { a: 1, b: 2, c: 3, SYM: 4};
+console.log(o2.SYM);
+console.log(o2['SYM']);
+console.log(o2[SYM]);
+
+const o3 = { a: 1, b: 2, c: 3 };
+o3[SYM] = 4;
+console.log(o3.SYM);
+console.log(o3['SYM']);
+console.log(o3[SYM]);
