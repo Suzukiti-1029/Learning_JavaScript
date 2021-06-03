e05cadcfe5a41f7ca0b0801820555deae0f7c0cc
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-03 09:49:22 +0900

    Object.keys

---
+++ es6/chapter9/ex09-01/main02-2.js
@@ -0,0 +1,7 @@
+const o = {
+  apple: 1, xochitl: 2, balloon: 3, guitar: 4, Xylophone: 5,
+};
+
+Object.keys(o)
+  .filter(prop => prop.match(/^x/))
+  .forEach(prop => console.log(`${prop}: ${o[prop]}`));
