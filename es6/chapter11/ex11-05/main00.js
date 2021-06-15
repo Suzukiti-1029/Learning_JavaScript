746fc14488ebee9c4dbb0a6fd4c2bc4116bd859b
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-15 19:21:06 +0900

    try...catch...finally

---
+++ es6/chapter11/ex11-05/main00.js
@@ -0,0 +1,10 @@
+try {
+  console.log('1行目実行中...');
+  throw new Error('エラー1'); /* この行をコメントにすると下の行が実行される */
+  console.log('throwが実行されると、この行は実行されない...');
+} catch (err) {
+  console.log('エラーが起こった...');
+} finally {
+  console.log('finallyの中のこの行はいつも実行される');
+  console.log('リソースの解放をここで行う');
+}
