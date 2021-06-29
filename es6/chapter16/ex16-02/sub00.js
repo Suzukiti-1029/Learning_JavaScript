c5849d64084ce8aa5ad4eb814a429511c85efc00
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-29 11:10:17 +0900

    より細かなフォーマット指定/定数

---
+++ es6/chapter16/ex16-02/sub00.js
@@ -0,0 +1,10 @@
+console.log(Math.E); // 自然対数の底
+console.log(Math.PI); // 円の周長の直径に対する比率(円周率)
+
+console.log(Math.LN2); // 2の自然対数
+console.log(Math.LN10); // 10の自然対数
+console.log(Math.LOG2E); // 2を底とするMath.Eの対数
+console.log(Math.LOG10E); // 10を底とするMath.Eの対数
+
+console.log(Math.SQRT1_2); // 1/2の平方根
+console.log(Math.SQRT2); // 2の平方根
