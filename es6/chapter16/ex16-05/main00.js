1a0ef17c8f21d5aae17196c9a46dfa8f7474a695
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-29 12:12:59 +0900

    双曲線関数

---
+++ es6/chapter16/ex16-05/main00.js
@@ -0,0 +1,17 @@
+console.log(Math.sinh(0));
+console.log(Math.sinh(1));
+
+console.log(Math.cosh(0));
+console.log(Math.cosh(1));
+
+console.log(Math.tanh(0));
+console.log(Math.tanh(1));
+
+console.log(Math.asinh(0));
+console.log(Math.asinh(1));
+
+console.log(Math.acosh(0));
+console.log(Math.acosh(1));
+
+console.log(Math.atanh(0));
+console.log(Math.atanh(1));
