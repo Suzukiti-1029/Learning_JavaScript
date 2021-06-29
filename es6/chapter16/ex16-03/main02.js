34bf9bdbd3872350e19c5fbc7e4c3ce77f7d9c14
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-29 11:20:31 +0900

    対数関連の関数

---
+++ es6/chapter16/ex16-03/main02.js
@@ -0,0 +1,11 @@
+console.log(Math.log(Math.E));
+console.log(Math.log(17.5));
+
+console.log(Math.log10(10));
+console.log(Math.log10(16.7));
+
+console.log(Math.log2(2));
+console.log(Math.log2(5));
+
+console.log(Math.log1p(Math.E - 1));
+console.log(Math.log1p(17.5));
