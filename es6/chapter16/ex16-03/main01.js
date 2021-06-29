a18f810f6c6ccc1c780af96ab92ee09b507e461f
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-29 11:16:47 +0900

    代数関連の関数/累乗

---
+++ es6/chapter16/ex16-03/main01.js
@@ -0,0 +1,17 @@
+console.log(Math.pow(2, 3));
+console.log(Math.pow(1.7, 2.3));
+
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(15.5));
+
+console.log(Math.cbrt(27));
+console.log(Math.cbrt(22));
+
+console.log(Math.exp(1));
+console.log(Math.exp(5.5));
+
+console.log(Math.expm1(1));
+console.log(Math.expm1(5.5));
+
+console.log(Math.hypot(3, 4));
+console.log(Math.hypot(2, 3, 4));
