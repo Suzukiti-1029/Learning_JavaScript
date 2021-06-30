78cc712141ccfe39c0c2d4196c13f13a58c590b6
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-06-30 12:25:32 +0900

    正規表現を用いた検索

---
+++ es6/chapter17/ex17-03/main00.js
@@ -0,0 +1,23 @@
+const input = 'As I was going to Saint Ives';
+            /* 01234567891123456789212345678 */
+const re = /\w{3,}/ig;
+
+console.log(input.match(re));
+console.log(input.search(re));
+
+console.log(re.test(input));
+
+let a = re.exec(input);
+console.log(a);
+console.log(a[0]);
+console.log(a.index);
+console.log(a.input);
+console.log(a[1]);
+
+console.log(re.exec(input));
+console.log(re.exec(input));
+console.log(re.exec(input));
+
+console.log(input.match(/\w{3,}/ig));
+console.log(input.match(/\w{3,}/i));
+console.log(input.search(/\w{3,}/ig));
