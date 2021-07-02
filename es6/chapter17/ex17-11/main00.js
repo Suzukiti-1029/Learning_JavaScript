43ca333a37c486771bd413626801d09678f690da
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-07-02 11:17:37 +0900

    ワイルドカード

--- es6/chapter17/ex17-11/main00.js
+++ es6/chapter17/ex17-11/main00.js
@@ -5,3 +5,9 @@ console.log(match[0]);
 const equation = '(2 + 3.5) * 7';
 const match2 = equation.match(/\(\d \+ \d\.\d\) \* \d/);
 console.log(match2[0]);
+
+const beer99 = '99 bottles of beer on the wall \n'
+  + 'take 1 down and pass it around -- \n'
+  + '98 bottles of beer on the wall.\n';
+const match3 = beer99.match(/[\s\S]*/);
+console.log(match3[0] === beer99);
