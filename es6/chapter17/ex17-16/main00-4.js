65754def8e5d497412e334eeee82ee4ee2eebfb4
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-07-03 11:44:25 +0900

    関数を用いた置換

---
+++ es6/chapter17/ex17-16/main00-4.js
@@ -0,0 +1,14 @@
+const html =
+  `〇〇<a onclick="alert('!!')" class="cl1" href="/foo" id="id1">XXX</a>△△`;
+
+const r = html.replace(/<a .*?<\/a>/ig, sanitizeATag);
+console.log(r);
+
+function sanitizeATag(aTag) {
+  const parts = aTag.match(/<a\s+(.*?)>(.*?)<\/a>/i);
+  console.log(`parts[1]=${parts[1]}`);
+  console.log(`parts[2]=${parts[2]}`);
+  const attributes = parts[1].split(/\s+/);
+  console.log(attributes);
+  return `<a ${attributes.filter((attr) => /^(?:class|id|href)[\s=]/i.test(attr)).join(' ')}>${parts[2]}</a>;`;
+}
---
+++ es6/chapter17/ex17-16/sub00.js
@@ -0,0 +1,5 @@
+const html =
+  `<a class="foo" href="/foo" id="foo">Foo</a>\n`
+  + `<A href='/foo' Class="foo">Foo</A>\n`
+  + `<a href="/foo">Foo</a>\n`
+  + `<a onclick="javascript:alert('foo!')" href="/foo">Foo</a>`;
